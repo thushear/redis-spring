f23e5a491f4830acad83f27837c4e5b0fa28da12
kongming <3708812046@163.com>
2014-05-23 19:24:46 +0800

    solr

---
+++ src/main/java/net/shopin/perf4j/Perf4JUtil.java
@@ -0,0 +1,95 @@
+package net.shopin.perf4j;
+
+import org.apache.log4j.Logger;
+import org.perf4j.LoggingStopWatch;
+import org.perf4j.StopWatch;
+import org.perf4j.log4j.Log4JStopWatch;
+
+/**
+ * 说明:
+ * User: kongming
+ * Date: 14-5-22
+ * Time: 下午10:58
+ */
+public class Perf4JUtil {
+
+
+
+
+    /**
+     * @Methods Name main
+     * @Create In 2013-10-12 By kongm
+     * @param args
+     *            void
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 5; i++) {
+            // 创建LoggingStopWatch开始计时,构造器中包含tag,标记了被计时的代码块
+            // 通常需要实例化LoggingStopWatch的一个子类例如Log4JStopWatch 或者
+            // CommonsLogStopWatch
+            StopWatch stopWatch = new LoggingStopWatch("codeblock1");
+            // 执行代码块 延迟0-1秒
+            Thread.sleep((long) (Math.random() * 1000L));
+            // 停表并记录,调用stop()或lap()方法LoggingStopWatch自动的记录执行时间
+            stopWatch.stop();
+            // 代码执行结果 进入system.err流
+                        /*
+                         * start[1381564569686] time[709] tag[codeblock1]
+                         * start[1381564570396] time[769] tag[codeblock1]
+                         * start[1381564571166] time[13] tag[codeblock1]
+                         * start[1381564571180] time[775] tag[codeblock1]
+                         * start[1381564571956] time[455] tag[codeblock1]
+                         */
+        }
+
+        // 此外,在stopWatch的tag中可以指定一些可选信息,这些tag或者message
+        //可以在StopWatch的构造器或者在stop方法中写明,在程序执行时tag可变.
+        StopWatch stopWatch = new LoggingStopWatch();
+        for(int i = 0;i < 10;i++){
+            try {
+                //用来做计时测试的伪码
+                long sleepTime = (long) (Math.random() * 1000L);
+                Thread.sleep(sleepTime);
+                if(sleepTime > 500L){
+                    throw new Exception("Throwing exception");
+                }
+                stopWatch.stop("codeBlock2.success","sleep time was < 500ms");
+            } catch (Exception e) {
+                stopWatch.stop("codeblock2.failure","Exception is : " + e);
+            }
+        }
+
+
+        Logger rootLogger = Logger.getRootLogger();
+
+        for(int i=0;i < 10;i++){
+            //默认的Log4JStopWatch会使用名为org.perf4j.TimeLogger的logger
+            stopWatch = new Log4JStopWatch();
+            //demo dummy code
+            Thread.sleep((long) (Math.random() * 1000L));
+            rootLogger.info("Normal logging message only go to the console");
+            //调用lap方法停止先前代码的计时,发送message到log4j的Logger并开始
+            //下个代码块的计时
+            stopWatch.lap("firstblock");
+            Thread.sleep((long) (Math.random() * 2000L));
+            stopWatch.stop("secondblock");
+        }
+
+        //上述代码日志输出
+                /*INFO: start[1230493236109] time[447] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493236719] time[567] tag[codeBlock2.failure] message[Exception was: java.lang.Exception: Throwing exception]
+                                INFO: start[1230493237286] time[986] tag[codeBlock2.failure] message[Exception was: java.lang.Exception: Throwing exception]
+                                INFO: start[1230493238273] time[194] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493238467] time[463] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493238930] time[310] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493239241] time[610] tag[codeBlock2.failure] message[Exception was: java.lang.Exception: Throwing exception]
+                                INFO: start[1230493239852] time[84] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493239937] time[30] tag[codeBlock2.success] message[Sleep time was < 500 ms]
+                                INFO: start[1230493239968] time[852] tag[codeBlock2.failure] message[Exception was: java.lang.Exception: Throwing
+*/
+    }
+
+
+
+}
--- src/main/java/net/shopin/solr/SolrUtil.java
+++ src/main/java/net/shopin/solr/SolrUtil.java
@@ -1,19 +1,18 @@
 package net.shopin.solr;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.beans.Field;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
-import org.apache.solr.core.CoreContainer;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * 说明:
@@ -27,6 +26,9 @@ public class SolrUtil {
 
     public static EmbeddedSolrServer eServer;
 
+
+    public static CommonsHttpSolrServer solrServer;
+
     static {
 
 
@@ -34,15 +36,21 @@ public class SolrUtil {
     }
 
 
+    public static void setUp() {
+        //commonshttpSolrServer
+        try {
+            solrServer = new CommonsHttpSolrServer("http://localhost:8983/solr");
+            solrServer.setSoTimeout(1000);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
 
-
-    public static void setUp(){
-        System.setProperty("solr.solr.home","D:\\solr-4.8.0\\example\\solr");
+        /*System.setProperty("solr.solr.home","D:\\solr-4.8.0\\example\\solr");
 
         CoreContainer.Initializer  initializer =  new CoreContainer.Initializer();
         try {
-            /*CoreContainer coreContainer = initializer.initialize();*/
+            *//*CoreContainer coreContainer = initializer.initialize();*//*
             File home = new File( "D:\\solr-4.8.0\\example\\solr" );
             File f = new File(home,"solr.xml");
             CoreContainer coreContainer = new CoreContainer();
@@ -56,15 +64,15 @@ public class SolrUtil {
         } catch (SAXException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
+*/
 
     }
 
 
-    public static void testAddIndex(){
+    public static void testAddIndex() {
         SolrInputDocument doc = new SolrInputDocument();
-        doc.setField("id","ABC");
-        doc.setField("text","京东商城");
+        doc.setField("id", "ABC");
+        doc.setField("text", "京东商城");
         try {
             eServer.add(doc);
             eServer.commit();
@@ -77,24 +85,22 @@ public class SolrUtil {
     }
 
 
-    public static void addIndex(){
+    public static void addIndex() {
         try {
 
 
-
-            CommonsHttpSolrServer solrServer = new CommonsHttpSolrServer("http://localhost:8393/solr");
-            solrServer.setSoTimeout(1000);
             SolrInputDocument doc1 = new SolrInputDocument();
-            doc1.addField( "id", "id1", 1.0f );
-            doc1.addField( "name", "doc1", 1.0f );
-            doc1.addField( "price", 10 );
+            doc1.addField("id", "id1", 1.0f);
+            doc1.addField("name", "doc1", 1.0f);
+            doc1.addField("price", 10);
+            doc1.addField("content", "中国人民站起来了好多年");
             SolrInputDocument doc2 = new SolrInputDocument();
-            doc2.addField( "id", "id2", 1.0f );
-            doc2.addField( "name", "doc2", 1.0f );
-            doc2.addField( "price", 20 );
+            doc2.addField("id", "id2", 1.0f);
+            doc2.addField("name", "doc2", 1.0f);
+            doc2.addField("price", 20);
             Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
-            docs.add( doc1 );
-            docs.add( doc2 );
+            docs.add(doc1);
+            docs.add(doc2);
             solrServer.add(docs);
             solrServer.commit();
         } catch (MalformedURLException e) {
@@ -109,23 +115,136 @@ public class SolrUtil {
     }
 
 
+    public static void deleteIndex(String ids) {
+        try {
+            solrServer.deleteById(ids);
+        } catch (SolrServerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
-    public static void main(String[] args){
 
-        System.out.println("solrj  start  ");
-        setUp();
-        testAddIndex();
-        /*addIndex();*/
+    public static void testQuery() {
+        SolrQuery query = new SolrQuery();
+        query.set("q", "*:*");
+        query.addSortField("price", SolrQuery.ORDER.asc);
+        try {
+            QueryResponse response = solrServer.query(query);
+            SolrDocumentList list = response.getResults();
+            List<Item> beans = response.getBeans(Item.class);
+            for (ListIterator iter = beans.listIterator(); iter.hasNext(); ) {
+                Item item = (Item) iter.next();
+                System.out.println(item);
+            }
 
+            /*for(int i = 0;i < list.size();i++){
+                SolrDocument sd = list.get(i);
+                String id = (String) sd.getFieldValue("id");
+                String text = (String) sd.getFieldValue("text");
+                System.out.println(id + ":" + text);
+            }*/
+
+
+        } catch (SolrServerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+
+    /**
+     * 测试高亮
+     */
+    public static void testHighLight() {
+        SolrQuery query = new SolrQuery();
+        //query.set("q","*:*");
+        query.setQuery("javaone");
+        query.setHighlight(true).setHighlightSnippets(1);
+        query.setParam("hl.fl", "content");
+        //query.addHighlightField("content");
+        //query.setHighlightSimplePre();
+        try {
+            QueryResponse response = solrServer.query(query);
+            Iterator<SolrDocument> iter = response.getResults().iterator();
+            while(iter.hasNext()){
+                SolrDocument resultDoc = iter.next();
+                List content = (List) resultDoc.getFieldValue("content");
+                String id = (String) resultDoc.getFieldValue("id");
+                System.out.println(id + ":" + content);
+                if(response.getHighlighting().get(id)!=null){
+
+                    List<String> highlightSnippets =
+                            response.getHighlighting().get(id).get("content");
+                    System.out.println(highlightSnippets);
+                }
+            }
+
+        } catch (SolrServerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
 
     }
 
 
+    public static void testAddBeans() {
+        Item item = new Item();
+        item.id = "one";
+        item.name = "javaone";
+        //item.price=100.0;
+        item.content = "中国人民";
+        try {
+            solrServer.addBean(item);
+            solrServer.commit();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SolrServerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
 
+    public static void main(String[] args) {
+
+        System.out.println("solrj  start  ");
+        /*setUp();
+        testAddIndex();*/
+        /*addIndex();*/
+        /*deleteIndex("id1");*/
+        //testQuery();
+        /*addIndex();*/
+        /*testAddBeans();*/
+        testHighLight();
+    }
 
 
+    public static class Item {
+
+        @Field
+        String id;
+
+        @Field
+        String name;
+
+        @Field
+        Float price;
+
+        @Field
+        String content;
+
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", price='" + price + '\'' +
+                    ", content='" + content + '\'' +
+                    '}';
+        }
+    }
 
 
 }
