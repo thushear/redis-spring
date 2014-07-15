a4bfd7696ccbccb63e63ae216100b55bbbe5f40e
kongming <3708812046@163.com>
2014-07-15 21:53:33 +0800

    commons

--- src/main/java/net/shopin/jakarta/commons/lang/PoliticalCandidate.java
+++ src/main/java/net/shopin/jakarta/commons/lang/PoliticalCandidate.java
@@ -1,12 +1,16 @@
 package net.shopin.jakarta.commons.lang;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.Validate;
 import org.apache.commons.lang.builder.*;
+import org.apache.commons.lang.math.DoubleRange;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.apache.commons.lang.time.FastDateFormat;
+import org.apache.commons.lang.time.StopWatch;
 
 import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 /**
  * 说明: commons-lang包 学习
@@ -21,6 +25,10 @@ public class PoliticalCandidate {
     private Date dateOfBirth;
     private BigDecimal moneyRaised;
     private State homeState;
+
+
+    public static final DoubleRange LAT_RANGE = new DoubleRange(-90.0,90.0);
+
 
 
     public PoliticalCandidate(String firstName, String lastName, Date dateOfBirth, BigDecimal moneyRaised, State homeState) {
@@ -160,6 +168,119 @@ public class PoliticalCandidate {
         ArrayUtils.reverse(reversed);
         System.out.println("Original: " +  ArrayUtils.toString(array)) ;
         System.out.println("Reversed: " + ArrayUtils.toString(reversed));
+
+        //transform between objects arrays and primitie arrays
+
+        long[] primitiveArray = new long[]{12,100,133,2333};
+        Long[] objectArray = ArrayUtils.toObject(primitiveArray);
+        System.out.println("object array:" +  objectArray);
+
+        //finding items in an array
+        String[] stringArray = {"Red","Blue","Brown","Red","Orange"};
+        boolean containsBlue = ArrayUtils.contains(stringArray,"Blue");
+        System.out.println(containsBlue);
+        int indexOfRed = ArrayUtils.indexOf(stringArray,"Red");
+        System.out.println(indexOfRed);
+        int lastIndexOfRed = ArrayUtils.lastIndexOf(stringArray,"Red");
+        System.out.println(lastIndexOfRed);
+
+        //多维数组构造Map
+        Object[] weightArray = new Object[][]{
+                {"H",new Double(1.007)},
+                {"He",new Double(4.002)}
+
+        };
+        //create a map
+        Map weights = ArrayUtils.toMap(weightArray);
+        Double h = (Double) weights.get("H");
+        System.out.println("h= " +  h);
+
+        //格式化时间
+        Date now = new Date();
+        String isoDT = DateFormatUtils.ISO_DATETIME_FORMAT.format(now);
+        System.out.println("It is currently:" + isoDT);
+        //自定义格式
+        //FastDateFormat formatter = new FastDateFormat("yyyy-mm-dd hh24:mi:ss", TimeZone.getDefault(),Locale.getDefault());
+        FastDateFormat  fastDateFormat = FastDateFormat.getInstance("yyyy-mm-dd");
+        String output = fastDateFormat.format(new Date());
+        System.out.println(output);
+
+        //时间工具类
+        FastDateFormat dtFormat = DateFormatUtils.ISO_DATETIME_FORMAT;
+        Date current = new Date();
+        Date nearestHour = DateUtils.round(current,Calendar.HOUR);
+        Date nearetDay = DateUtils.round(current,Calendar.DAY_OF_MONTH);
+        Date nearestYear = DateUtils.round(current,Calendar.YEAR);
+        System.out.println("Now:" + dtFormat.format(current));
+        System.out.println("nearest hour :" + dtFormat.format(nearestHour));
+        System.out.println("nearest day:" + dtFormat.format(nearetDay));
+        System.out.println("nearest year:" + dtFormat.format(nearestYear));
+
+        //id 生成器
+
+
+        //入参验证
+        //doSomeThing(-1,null,null);
+        //doSomeThing(1,new Object[]{},null);
+
+        List list =  Collections.synchronizedList(new ArrayList());
+        list.add(null);
+        //doSomeThing(1, new Object[]{1}, list);
+       // doSomeThing2(-1000d);
+
+        //计时
+        StopWatch clock = new StopWatch();
+        System.out.println("Time to Math.sin(0.34 ) ten million times");
+        clock.start();
+        for (int i=0;i<10000000;i++){
+            Math.sin(0.34);
+        }
+        clock.stop();
+        System.out.println("it takes " + clock.getTime() + " milliseconds");
+
+        //split
+        clock.reset();
+        clock.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        clock.split();
+        System.out.println("Split time after 1 sec :" + clock.getTime());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("Split time after 2 sec:"  + clock.getTime());
+        clock.unsplit();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("Time after 3 sec:" + clock.getTime());
+
+    }
+
+    public static void doSomeThing2(Double low){
+        Validate.isTrue(LAT_RANGE.containsDouble(low));
+    }
+
+
+    /**
+     * 入参验证
+     * @param param1
+     * @param param2
+     * @param param3
+     */
+    public static void doSomeThing(int param1,Object[] param2,Collection param3){
+        Validate.isTrue(param1>0,"param must be greater than zero");
+        Validate.notEmpty(param2,"param2 must not be empty");
+        Validate.noNullElements(param3);
+
 
     }
 
